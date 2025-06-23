23739a446f226114a1484a693996d401e9385b7b
ManuelFBS <manuelf.borrego@gmail.com>
2025-06-23 03:08:07 -0400

    Creación de las rutas del User...

--- src/interfaces/routes/user/user.routes.ts
+++ src/interfaces/routes/user/user.routes.ts
@@ -1,3 +1,4 @@
+/*
 import { Router } from 'express';
 import { UserController } from '../../controllers/User.Controller';
 import { UserRepositoryImpl } from '../../../infrastructure/repositories/UserRepositoryImpl';
@@ -49,6 +50,70 @@ router.get(
     authenticate,
     asyncHandler(authorize(['user:read'])),
     userController.getUserByDni.bind(userController),
+);
+
+export default router;
+*/
+import { Router } from 'express';
+import { UserController } from '../../controllers/User.Controller';
+import {
+    CreateUserDTO,
+    UpdateUserDTO,
+    LoginDTO,
+} from '../../dtos/UserDTO';
+import { validationMiddleware } from '../../middlewares/validationMiddleware';
+import {
+    authenticate,
+    authorize,
+} from '../../middlewares/authMiddleware';
+import {
+    apiLimiter,
+    authLimiter,
+} from '../../middlewares/rateLimiter';
+
+const router = Router();
+const userController = new UserController();
+
+//~ Rutas públicas...
+router.post(
+    '/login',
+    authLimiter,
+    validationMiddleware(LoginDTO),
+    userController.login.bind(userController),
+);
+
+//~ Rutas autenticadas...
+router.post(
+    '/newuser',
+    authenticate,
+    authorize(['user:create']),
+    apiLimiter,
+    validationMiddleware(CreateUserDTO),
+    userController.createUser.bind(userController),
+);
+
+router.put(
+    '/:dni',
+    authenticate,
+    authorize(['user:update']),
+    apiLimiter,
+    validationMiddleware(UpdateUserDTO),
+    userController.updateUser.bind(userController),
+);
+
+router.delete(
+    '/:dni',
+    authenticate,
+    authorize(['user:delete']),
+    apiLimiter,
+    userController.deleteUser.bind(userController),
+);
+
+router.get(
+    '/:dni',
+    authenticate,
+    authorize(['user:create']),
+    userController.getUserByDNI.bind(userController),
 );
 
 export default router;
